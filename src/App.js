2ad954d6c0c2469bd057a3b600773503307e5dbc
Jan-Geert Munneke <477843+jngrt@users.noreply.github.com>
2024-05-22 14:02:13 +0200

    try with node 18, azure functions 4

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState('');
-  const [error, setError] = useState('');
+  const [data, setData] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     (async function () {
       try {
-        const response = await fetch(`/api/message`);
-        console.log('Response:', response);
+        const response = await fetch(`/api/hello?name=Random`);
+        console.log("Response:", response);
 
-        const contentType = response.headers.get('Content-Type');
-        console.log('Content-Type:', contentType);
+        const contentType = response.headers.get("Content-Type");
+        console.log("Content-Type:", contentType);
 
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         const textResponse = await response.text();
-        console.log('Text Response:', textResponse);
+        console.log("Text Response:", textResponse);
 
         try {
           const jsonResponse = JSON.parse(textResponse);
           const { text } = jsonResponse;
 
-          if (typeof text === 'string') {
+          if (typeof text === "string") {
             setData(text);
           } else {
             console.log("unexpected data type", typeof text);
             // Handle unexpected data type
           }
         } catch (parseError) {
-          console.error('Error parsing JSON:', parseError);
-          setError('Error parsing JSON');
+          console.error("Error parsing JSON:", parseError);
+          setError("Error parsing JSON");
         }
       } catch (fetchError) {
-        console.error('Fetch error:', fetchError);
-        setError('Fetch error');
+        console.error("Fetch error:", fetchError);
+        setError("Fetch error");
       }
     })();
   }, []);
 
-  return (
-    <div>
-      {data || error}
-    </div>
-  );
+  return <div>{data || error}</div>;
 }
 
 export default App;
