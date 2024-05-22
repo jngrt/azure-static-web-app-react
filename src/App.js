cd79c68a47acf2ea1352f6c5aebb2c3fca218d27
Jan-Geert <477843+jngrt@users.noreply.github.com>
2024-05-22 11:49:33 +0200

    add backend http function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const response = await fetch(`/api/message`);
+        console.log('Response:', response);
+
+        const contentType = response.headers.get('Content-Type');
+        console.log('Content-Type:', contentType);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const textResponse = await response.text();
+        console.log('Text Response:', textResponse);
+
+        try {
+          const jsonResponse = JSON.parse(textResponse);
+          const { text } = jsonResponse;
+
+          if (typeof text === 'string') {
+            setData(text);
+          } else {
+            console.log("unexpected data type", typeof text);
+            // Handle unexpected data type
+          }
+        } catch (parseError) {
+          console.error('Error parsing JSON:', parseError);
+          setError('Error parsing JSON');
+        }
+      } catch (fetchError) {
+        console.error('Fetch error:', fetchError);
+        setError('Fetch error');
+      }
+    })();
+  }, []);
+
+  return (
+    <div>
+      {data || error}
+    </div>
+  );
 }
 
 export default App;
